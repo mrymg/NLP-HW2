b90445cbc70f8c929fa6070500dd2371bfc53e82
Yunus Melih Gözütok <ymgozutok@gmail.com>
2019-04-12 21:19:03 +0300

    sentenceList eklendi

--- NLP-2.py
+++ NLP-2.py
@@ -1,7 +1,9 @@
 from collections import Counter
 import math
 import numpy as np
-
+from operator import itemgetter
+import json
+LOW_PROB = float(0.0000000000001)
 print("-----------\n TASK 1\n-----------")
 filePath = open("metu.txt", "r", encoding="utf-8")
 read = filePath.read()
@@ -18,7 +20,6 @@ countBeginTags=[]
 for sentence in dataSet[:3960]:
    for tags in sentence.split():
        taggim.append(tags.split("/")[1])
-       wordList.append(tags.split("/")[0])
 
 
 countTaggim = Counter(taggim)
@@ -52,6 +53,7 @@ for y in countBigramTags:
 for y in countBigramTags:
     transitionProbability[y.split()[0]].update({y.split()[1] : countBigramTags[y]/countTaggim[y.split()[0]]})
 
+
 print("Transition Probability Dict is" , transitionProbability)
 
 
@@ -74,4 +76,86 @@ print("-----------\n TASK 2 \n-----------")
 # -----------------------------------
 
 
-sentenceList = [x.split() for x in dataSet]
+sentenceList = []
+
+for i in dataSet[3960:]:
+    sentencim = []
+    for s in i.split():
+        sentencim.append(s.split("/")[0])
+    sentenceList.append(sentencim)
+
+def bigram_viterbi(observation, tags, start_prob, transition_prob, emission_prob):
+    viterbiMatrix = {}
+
+    backpointer = {}
+    backpointers=[]
+    probabilities =[]
+    observation_length = len(observation)
+    for tag in tags:
+        firstWord = observation[0]
+        viterbiMatrix[0, tag] = (start_prob.get(tag, 0) * emission_prob[tag].get(firstWord, LOW_PROB))
+        backpointer[0, tag] = None
+    backpointers.append(backpointer)
+    probabilities.append(viterbiMatrix)
+    # first word given
+    # ================================= Viterbi 1 =================================================
+
+    for time, word in enumerate(observation[1:], start=1):
+        probs = {}
+        for tag in tags:
+            maxProbs = []
+
+            for prevTag, prevProb in probabilities[time-1].items():
+                probability={(time, tag):prevProb* transitionProbability.get(prevTag[1],0).get(tag, LOW_PROB) * emmisionProbability.get(tag, 0).get(word, LOW_PROB)}
+                maxProbs.append((prevTag, probability))
+
+
+
+            # max probsun içinde en yüksek olasılıklıyı bul probsa ekle probsun içinde backpointer ın yanına da koyabilirsin
+
+        quit()
+
+        #================================= Viterbi 2 =================================================
+
+    # for time_step, word in enumerate(observation[1:], start=1):
+    #     for tag in tags:
+    #         # max probability for the viterbi matrix
+    #         viterbiMatrix[time_step, tag] = max(
+    #             viterbiMatrix[time_step - 1, prev_tag] *
+    #             transition_prob.get((tag, prev_tag), 0) *
+    #             emission_prob.get((word, tag), 0) for prev_tag in tags)
+    #         # argmax for the backpointer
+    #         probability, state = max(
+    #             (viterbiMatrix[time_step - 1, prev_tag] *
+    #              transition_prob.get((tag, prev_tag), 0),
+    #              prev_tag) for prev_tag in tags)
+    #         backpointer[time_step, tag] = state
+    #
+    #         # Termination steps
+    #         viterbiMatrix[observation_length, "Punc"] = max(viterbiMatrix.get((observation_length - 1, tag), 0) * transition_prob.get((tag, "Punc"), 0) for tag in tags)
+    #         probability, state = max(
+    #             (viterbiMatrix.get((observation_length - 1, tag), 0) *
+    #              transition_prob.get((tag, "Punc"), 0), tag) for tag in tags)
+    #         backpointer[observation_length, "Punc"] = state
+    #
+    #         # Return backtrace path...
+    #         backtrace_path = []
+    #         previous_state = "Punc"
+    #         for index in range(observation_length, 0, -1):
+    #             state = backpointer.get((index, previous_state),0)
+    #             backtrace_path.append(state)
+    #             previous_state = state
+    #         # We are tracing back through the pointers, so the path is in reverse
+    #         backtrace_path = list(reversed(backtrace_path))
+    #         return backtrace_path
+
+
+
+
+
+
+
+
+taggim = list(set(taggim))
+for sentence in sentenceList:
+    print(bigram_viterbi(sentence, taggim, startProbability, transitionProbability, emmisionProbability) )
